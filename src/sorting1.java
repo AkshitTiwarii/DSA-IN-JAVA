98e04aa5d550ad974ce2e443d3c518808a40f913
Akshit Tiwari <akshittiwari29@gmail.com>
2026-02-04 01:45:28 +0530

    4 Feb 2026

--- src/sorting1.java
+++ src/sorting1.java
@@ -4,11 +4,12 @@ import java.util.Arrays;
 public class sorting1 {
     public static void main(String[] args){
         int arr[] = {8,6,9,2,4,5};
-        bubble(arr);
+        selection(arr);
     }
+    // O(n^2)
     static void bubble(int arr[]){
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length-1; j++) {
+            for (int j = 0; j < arr.length-i-1; j++) {
                 if(arr[j]>arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
@@ -17,5 +18,27 @@ public class sorting1 {
             }
         }
         System.out.println(Arrays.toString(arr));
+    }
+    static void selection(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            int minIndex = i;  // assume first element is minimum
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // swap
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+    static void insertion(int[] arr){
+
     }
 }
